3a717de752fa08762fec79a858df2294712c36cc
jeanporro <jporro40@gmail.com>
2023-08-17 21:15:59 +0200

    structuredone

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+resources :lists, only: [:index, :show, :create]
+resources :bookmarks, only: [:index, :show, :create]
+resources :movies, only: [:index, :show, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
